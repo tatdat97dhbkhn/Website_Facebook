983c151d363987cbe3f065d53f540b1f69f7aa4c
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-09-15 15:30:21 +0700

    google_facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable, :confirmable
+    :recoverable, :rememberable, :validatable, :confirmable,
+    :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   has_many :news, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -21,6 +22,27 @@ class User < ApplicationRecord
   validates :name, presence: true,
     length: {maximum: Settings.user.length.max_name}
   validate  :image_size
+
+  def self.new_with_session params, session
+    super.tap do |user|
+      if data = session["devise.facebook_data"] &&
+        session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.image = URI.parse auth.info.image
+      user.skip_confirmation!
+      user.confirm
+      user.save!
+    end
+  end
 
   private
   def image_size
